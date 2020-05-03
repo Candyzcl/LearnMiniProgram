69bea261b718ff43d1b8fc0a7e0ba9480ca82dc5
Candyzcl <2596924715@qq.com>
2020-05-03 14:22:06 +0800

    wxs/wxml

---
+++ pages/wxml/wxml.js
@@ -0,0 +1,47 @@
+// pages/wxml/wxml.js
+Page({
+
+  data: {
+    message:'你好小程序',
+    firstname:'kobe',
+    lastname:'bryant',
+    age:20,
+    nowTime:new Date().toLocaleString(),
+    isActive:false,
+    isShow:true,
+    score:45,
+    movies:['星际穿越','盗梦空间','大话西游'],
+    nums:[
+      [10, 14, 11, 18],
+      [110, 114, 111, 118],
+      [210, 214, 211, 218]
+    ],
+    letters:['a','b','c']
+  },
+  onLoad(){
+    setInterval(()=>{
+      this.setData({
+        nowTime: new Date().toLocaleString()
+      })
+    },1000)
+  },
+  handleSwitchColor(){
+    this.setData({
+      isActive:!this.data.isActive
+    })
+  },
+  handleSwitchShow(){
+    this.setData({
+      isShow:!this.data.isShow
+    })
+  },
+  handleIncrement(){
+    this.setData({
+      score:this.data.score + 6
+    })
+  },
+  numberFoFixed(value){
+    return value.toFixed(2)
+  }
+
+})
